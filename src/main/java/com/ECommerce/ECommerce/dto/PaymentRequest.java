ec011baf68ec8abf9d0a16186a466e744c445361
IT24101369 <samadhiweerakkodi23@gmail.com>
2026-03-25 22:45:35 +0530

    Update src/main/java/com/ECommerce/ECommerce/dto/PaymentRequest.java

---
+++ src/main/java/com/ECommerce/ECommerce/dto/PaymentRequest.java
@@ -0,0 +1,62 @@
+package com.ECommerce.ECommerce.dto;
+
+public class PaymentRequest {
+
+    private String fullName;
+    private String email;
+    private String address;
+    private String cardNumber;
+    private String expiry;
+    private String cvv;
+
+    public PaymentRequest() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+}
