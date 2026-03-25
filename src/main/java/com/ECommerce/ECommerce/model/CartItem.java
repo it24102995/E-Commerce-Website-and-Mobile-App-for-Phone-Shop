2548642149f2436cb6ade88abfdededca1a59785
IT24101369 <samadhiweerakkodi23@gmail.com>
2026-03-25 22:45:40 +0530

    Update src/main/java/com/ECommerce/ECommerce/model/CartItem.java

---
+++ src/main/java/com/ECommerce/ECommerce/model/CartItem.java
@@ -0,0 +1,57 @@
+package com.ECommerce.ECommerce.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+    private String img;
+    private int quantity;
+
+    public CartItem() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
